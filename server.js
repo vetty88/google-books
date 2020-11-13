6d68e5365bbb055d39de78268e06da609adb68cf
Vetty <vetty88@gmail.com>
2020-11-13 18:32:51 +1100

    refreshed modules

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+const express = require("express");
 
+const mongoose = require("mongoose");
 const routes = require("./routes");
-const express  = require('express'), app = express(), mongoose = require('mongoose');
-require('dotenv').config(); // configures dotenv
-app.use(express.json());
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -14,19 +15,10 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-// MongoDB connection with ATLAS and Mongoose
-// connects to the value within the .env file
-const uri = process.env.ATLAS_URI;
-// connects mongoose to the uri and sets some mongoose keys to true to combat mongoose's deprecation warnings
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
-const connection = mongoose.connection;
-// make sure that MongoDB connected successfully
-connection.once('open', () => {
-console.log("MongoDB database connected!!");
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
-app.listen(3000, process.env.IP, () => {
-console.log("Server Started!");
-});
-
-
-
