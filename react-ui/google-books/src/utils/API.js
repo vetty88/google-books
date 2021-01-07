a08e4515e61dcb30e469acdad96f6ab9a5fabfe8
Vetty <vetty88@gmail.com>
2021-01-07 14:36:58 +1100

    update

--- react-ui/google-books/src/utils/API.js
+++ react-ui/google-books/src/utils/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default {
     // Get book from google search 
     getGoogleSearchBooks: function(query) {
-        return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query)
+        return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query);
     },
   // Gets all books
   getBooks: function() {
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
-const app = express();
+
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,20 +12,13 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
-// Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
-  {
-    useCreateIndex: true,
-    useNewUrlParser: true
-  }
-);
-
 // Add routes, both API and view
 app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
