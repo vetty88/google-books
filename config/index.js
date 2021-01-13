de099f611eff4283bc317d3269bb70dae722ce61
Vetty <vetty88@gmail.com>
2021-01-14 10:57:45 +1100

    starting build for mongodb connection

---
+++ config/index.js
@@ -0,0 +1,6 @@
+const dbuser = 'xxxxxxxxxx';
+const dbpassword = 'xxxxxxxxx';
+
+const MONGODB_URI = `mongodb://${dbuser}:${dbpassword}@ds125453.mlab.com:25453/mern-example`;
+
+module.exports = MONGODB_URI;
