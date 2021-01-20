f43b3093dafa53df1afbdfac941e55967593a2f5
Vetty <vetty88@gmail.com>
2021-01-21 10:29:26 +1100

    add node js update server file

--- react-ui/google-books/server/server.js
+++ react-ui/google-books/server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 if(process.env.NODE_ENV === 'production'){
   const path  =  require('path');
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
@@ -12,38 +12,3 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port);
-
-// const path = require('path');
-// const book = require('./models/book');
-// const express = require ("express");
-// const app = express();
-
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-// // serves the built version of your react app
-// app.use(express.static(path.join(__dirname, 'client/build')));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/client/build/index.html'));
-// });
-
-// // for your REST calls, append api to the beginning of the path
-// // ex: 
-// app.get('/api/books', async (req, res) => {
-//   try {
-//     res.json(await book.find());
-//     // book is a mongoose schema we've defined in /models
-//     // .find() is a method on the model for fetching all documents
-//   } catch (err) {
-//     res.json({message: err});
-//   }
-// });
-
-// // ensures the proxy we set earlier is pointing to your express api
-// const port = process.env.PORT || 5000;
-// app.listen(port, () => {
-//   console.log(`listening on port ${port}`);
-// });
