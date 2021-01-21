de9457df7130fe18e88bafeb06bf5e49028d1740
Vetty <vetty88@gmail.com>
2021-01-22 10:39:15 +1100

    remove buildpacks

--- react-ui/google-books/server/server.js
+++ react-ui/google-books/server/server.js
@@ -1,17 +1,8 @@
-const path = require('path');
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
-
-if(process.env.NODE_ENV === 'production'){
-  const path  =  require('path');
-  app.get('*', function (req, res) {
-    const index = path.join(__dirname, 'build', 'index.html');
-    res.sendFile(index);
-  });
-}
-
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
+app.get('*', function (req, res) {
+  res.sendFile('index.html');
 });
+
+app.listen(process.env.PORT || 8080);
