8d67055fdf054992faac33cc1dc9557df1dc5077
YVETTE WALLER <vetty88@gmail.com>
2021-02-09 08:45:58 +1100

    duplicate express

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || '8080';
 const app = express();
