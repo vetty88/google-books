9930dd5f74e04bae2f600a6960efdc42c5ba55ca
YVETTE WALLER <vetty88@gmail.com>
2021-02-08 17:43:01 +1100

    update server

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const express = require('express');
+const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(9000);
+
 // Add routes, both API and view
 app.use(routes);
 
