8ad6291bcfb09e9d9d851a290e25815bdc040c56
YVETTE WALLER <vetty88@gmail.com>
2021-02-08 18:32:26 +1100

    update remove buildpacks

--- client/server.js
+++ client/server.js
@@ -9,7 +9,7 @@ app.set("port", PORT)
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
