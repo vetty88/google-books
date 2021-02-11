98b8b3ba6d0fc741a22fde59e5f9bb435d6cbb3d
YVETTE WALLER <vetty88@gmail.com>
2021-02-12 10:41:36 +1100

    update client build

---
+++ client/build/precache-manifest.1c9a7ee63bcdf076d6c288295fcdd882.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9dcb8075932bac3b59a4",
+    "url": "/static/css/main.06ea5eae.chunk.css"
+  },
+  {
+    "revision": "9dcb8075932bac3b59a4",
+    "url": "/static/js/main.9dcb8075.chunk.js"
+  },
+  {
+    "revision": "674f15771914c51de75a",
+    "url": "/static/js/1.674f1577.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6e0a32278b18478a90bc2b7d4b7d005f",
+    "url": "/index.html"
+  }
+];
