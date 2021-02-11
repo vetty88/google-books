7c639bca526164fd16fc41bd894bba4837a61f6e
YVETTE WALLER <vetty88@gmail.com>
2021-02-11 14:57:56 +1100

    change relative url from double to single quotes

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const booksController = require("../../controllers/booksController");
 
-// Matches with "/api/books"
+// Matches with '/api/books'
 router.route("/")
   .get(booksController.findAll)
   .post(booksController.create);
