9247389c4f478286809aa5dbb49db6779d4b9493
YVETTE WALLER <vetty88@gmail.com>
2021-02-12 12:43:17 +1100

    rebuild app

---
+++ client/build/precache-manifest.a46ec6d49cdc66f5ad68b4adc11e40e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "819b05a9d00863727eba",
+    "url": "/static/css/main.06ea5eae.chunk.css"
+  },
+  {
+    "revision": "819b05a9d00863727eba",
+    "url": "/static/js/main.819b05a9.chunk.js"
+  },
+  {
+    "revision": "674f15771914c51de75a",
+    "url": "/static/js/1.674f1577.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "32a6162b50e4888647be564cbe822d75",
+    "url": "/index.html"
+  }
+];
