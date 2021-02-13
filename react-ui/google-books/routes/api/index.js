47bd66a35cc419b05a19ed0bde7f3e03d199b446
YVETTE WALLER <vetty88@gmail.com>
2021-02-13 16:25:51 +1100

    updated all incl react ui folder and rebuilt

--- react-ui/google-books/routes/api/index.js
+++ react-ui/google-books/routes/api/index.js
@@ -1,7 +1,17 @@
+const path = require("path");
 const router = require("express").Router();
-const bookRoutes = require("./books");
+const booksRoutes = require("./books");
+const googleRoutes = require("./google");
 
-// Book routes
-router.use("/books", bookRoutes);
+// Books routes match /api/books
+router.use("/books", booksRoutes);
 
-module.exports = router;
+// Google Routes match /api/books
+router.use("/google", googleRoutes);
+
+// For anything else, render the html page
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+});
+
+module.exports = router
--- react-ui/google-books/server.js
+++ react-ui/google-books/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const routes = require("./routes");
+const routes = require("./server/routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
